9170209bfb7be9eaf71028606a904fd3e9c8286e
Ackerley Tng <ackerleytng@gmail.com>
2022-03-02 16:08:35 +0800

    Use server's date instead of the date from an API

--- src/time.js
+++ src/time.js
@@ -2,32 +2,18 @@ import { useState, useEffect } from 'react';
 import moment from 'moment';
 
 /**
- * Gets the current time from online apis, returns a moment instance in utc mode
+ * Gets the current time from the server using the date header, returns a
+ * moment instance in utc mode
  *
- * Don't trust user's configured time to be right, hence query an api - we can
- * afford to be slightly off as long was we're approximately consistent
+ * Don't trust user's configured time to be right, hence query the server - we
+ * can afford to be slightly off as long was we're approximately consistent
  *
  * Fall back to user's configured time if none of the APIs can be reached
  */
-export const getTime = () => {
-  // In order of priority: first try the topmost API, then the next
-  const apis = [
-    { url: 'https://worldtimeapi.org/api/timezone/Etc/UTC', propName: 'utc_datetime' },
-    { url: 'http://worldclockapi.com/api/json/utc/now', propName: 'currentDateTime' },
-  ];
-
-  const fetchRetry = (n = 0) => fetch(apis[n].url)
-        .then(r => [r, n])
-        .catch(err => {
-          if (n === apis.length) {
-            throw err;
-          }
-          return fetchRetry(n + 1);
-        });
-
-  return fetchRetry()
-    .then(([response, n]) => moment.utc(response.json()[apis[n]]))
-    .catch(err => moment.utc());
+export const getTime = async () => {
+  const response = await fetch('/', {method: 'HEAD'});
+  const dateString = response.headers.get('Date');
+  return moment.utc(dateString);
 }
 
 const scrapeTimeStorageAddr = 'https://jsonblob.com/api/jsonBlob/8adcab2c-63b1-11ea-ad21-457f7983555e';
