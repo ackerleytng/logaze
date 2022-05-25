5b17317f1b6ba2b7e1e5618f926d450bb8137dc3
Ackerley Tng <ackerleytng@gmail.com>
2022-05-25 08:45:43 -0700

    Update Grid for new data format

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -15,45 +15,44 @@ const renderDecimal = (decimalPlaces) => (value) => value.toFixed(decimalPlaces)
 
 const nullCheck = (fn) => ({ value }) => value === null ? value : fn(value);
 
-const prefixDollarSign = (fn) => (s) => `$${fn(s)}`;
+const price = (value) => `$${value}`;
 
 const Grid = ({ data }) => {
     const columnDefs = [
     {headerName: 'USD', field: 'url', width: 65, cellRenderer: nullCheck(BuyButton)},
     {headerName: 'Price', field: 'price', width: 70,
-     cellRenderer: nullCheck(prefixDollarSign(renderDecimal(2))),
+     cellRenderer: nullCheck(price),
      filter: 'agNumberColumnFilter', sort: 'asc'},
-    {headerName: 'Type', field: 'product-type', width: 70},
+    {headerName: 'Condition', field: 'product-condition', width: 70},
     {headerName: 'Model', field: 'model'},
     {headerName: 'Screen Size', field: 'screen-size', width: 70,
      cellRenderer: nullCheck(renderDecimal(1)),
      filter: 'agNumberColumnFilter'},
     {headerName: 'Resolution', field: 'resolution', width: 100},
     {headerName: 'IPS Screen?', field: 'screen-has-ips', width: 70},
-    {headerName: 'Display', field: 'display-type'},
+    {headerName: 'Display', field: 'display'},
     {headerName: 'Memory Size', field: 'memory-size', width: 70},
-    {headerName: 'Hard Drive Type', field: 'hard-drive-type', width: 70},
-    {headerName: 'Hard Drive Size', field: 'hard-drive-size', width: 70},
+    {headerName: 'Storage Type', field: 'storage-type', width: 70},
+    {headerName: 'Storage Size', field: 'storage-size', width: 70},
     {headerName: 'Processor Brand', field: 'processor-brand', width: 70},
     {headerName: 'Processor Range', field: 'processor-range', width: 80},
     {headerName: 'Processor', field: 'processor'},
-    {headerName: 'Wireless', field: 'wireless'},
+    {headerName: 'Wireless', field: 'wlan'},
     {headerName: 'Graphics', field: 'graphics'},
-    {headerName: 'Touchscreen?', field: 'screen-supports-touch', width: 70},
-    {headerName: 'Hard Drive', field: 'hard-drive'},
+    {headerName: 'Camera', field: 'camera'},
+    {headerName: 'Touchscreen?', field: 'touch-screen', width: 70},
+    {headerName: 'Storage', field: 'storage'},
     {headerName: 'Memory', field: 'memory'},
     {headerName: 'Memory Soldered?', field: 'memory-soldered', width: 70},
     {headerName: 'Processor Cache', field: 'processor-cache', width: 70},
     {headerName: 'Battery', field: 'battery'},
     {headerName: 'Warranty', field: 'warranty'},
+    {headerName: 'Weight', field: 'weight'},
     {headerName: 'Operating System', field: 'operating-system'},
-    {headerName: 'Original Price', field: 'orig-price', width: 70,
-     cellRenderer: nullCheck(prefixDollarSign(renderDecimal(2))),
-     filter: 'agNumberColumnFilter'},
-    {headerName: 'Pointing Device', field: 'pointing-device'},
-    {headerName: 'Part Number', field: 'part-number'},
+    {headerName: 'Original Price', field: 'orig-price', width: 70, filter: 'agNumberColumnFilter'},
+    {headerName: 'Fingerprint Reader', field: 'fingerprint-reader'},
+    {headerName: 'Product Number', field: 'product-number'},
     {headerName: 'Keyboard', field: 'keyboard'},
-    {headerName: 'Bluetooth', field: 'bluetooth', width: 70},
   ];
 
   const defaultColDef = {
