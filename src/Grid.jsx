1593c61f7fc883fde321c1d922f5e16dacb3797b
Ackerley Tng <ackerleytng@gmail.com>
2023-03-09 19:02:24 -0800

    Move columnDefs out of component

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -2,7 +2,7 @@ import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham-dark.css";
 
 import { AgGridReact } from "ag-grid-react";
-import React, { useMemo } from "react";
+import React from "react";
 
 const Buy = (value) => (
   <a target="_blank" rel="noopener noreferrer" href={value}>
@@ -13,10 +13,7 @@ const Buy = (value) => (
 const renderDecimal = (decimalPlaces) => (value) =>
   value.toFixed(decimalPlaces);
 
-const nullCheck =
-  (fn) =>
-  ({ value }) =>
-    value && fn(value);
+const nullCheck = (fn) => ({ value }) => value && fn(value);
 
 const defaultColDef = {
   sortable: true,
@@ -47,78 +44,75 @@ const storageComparator = (aStr, bStr) => {
   return a > b ? 1 : -1;
 };
 
-const Grid = ({ data }) => {
-  const columnDefs = useMemo(
-    () => [
-      { headerName: "", field: "url", width: 44, cellRenderer: nullCheck(Buy) },
-      {
-        headerName: "Price",
-        field: "price",
-        width: 75,
-        filter: "agNumberColumnFilter",
-        sort: "asc",
-        cellRenderer: nullCheck(renderDecimal(2)),
-      },
-      { headerName: "Condition", field: "product-condition", width: 70 },
-      { headerName: "Model", field: "model" },
-      {
-        headerName: "Screen Size",
-        field: "screen-size",
-        width: 70,
-        cellRenderer: nullCheck(renderDecimal(1)),
-        filter: "agNumberColumnFilter",
-      },
-      { headerName: "Resolution", field: "resolution", width: 100 },
-      { headerName: "IPS Screen?", field: "screen-has-ips", width: 70 },
-      { headerName: "Display", field: "display" },
-      {
-        headerName: "Memory Size",
-        field: "memory-size",
-        width: 70,
-        comparator: memComparator,
-      },
-      { headerName: "Storage Type", field: "storage-type", width: 70 },
-      {
-        headerName: "Storage Size",
-        field: "storage-size",
-        width: 70,
-        comparator: storageComparator,
-      },
-      { headerName: "Processor Brand", field: "processor-brand", width: 70 },
-      { headerName: "Processor Range", field: "processor-range", width: 80 },
-      { headerName: "Processor", field: "processor" },
-      { headerName: "Wireless", field: "wlan" },
-      { headerName: "Graphics", field: "graphic-card" },
-      { headerName: "Camera", field: "camera" },
-      { headerName: "Touchscreen?", field: "touch-screen", width: 70 },
-      { headerName: "Storage", field: "storage" },
-      { headerName: "Memory", field: "memory" },
-      { headerName: "Memory Soldered?", field: "memory-soldered", width: 70 },
-      { headerName: "Processor Cache", field: "processor-cache", width: 70 },
-      { headerName: "Battery", field: "battery" },
-      { headerName: "Warranty", field: "warranty" },
-      { headerName: "Weight", field: "weight" },
-      { headerName: "Operating System", field: "operating-system" },
-      {
-        headerName: "Original Price",
-        field: "orig-price",
-        width: 75,
-        filter: "agNumberColumnFilter",
-        cellRenderer: nullCheck(renderDecimal(2)),
-      },
-      {
-        headerName: "Percentage Savings",
-        field: "percentage-savings",
-        width: 75,
-        filter: "agNumberColumnFilter",
-      },
-      { headerName: "Fingerprint Reader", field: "fingerprint-reader" },
-      { headerName: "Product Number", field: "product-number" },
-      { headerName: "Keyboard", field: "keyboard" },
-    ],
-    []
-  );
+const columnDefs = [
+  { headerName: "", field: "url", width: 44, cellRenderer: nullCheck(Buy) },
+  {
+    headerName: "Price",
+    field: "price",
+    width: 75,
+    filter: "agNumberColumnFilter",
+    sort: "asc",
+    cellRenderer: nullCheck(renderDecimal(2)),
+  },
+  { headerName: "Condition", field: "product-condition", width: 70 },
+  { headerName: "Model", field: "model" },
+  {
+    headerName: "Screen Size",
+    field: "screen-size",
+    width: 70,
+    cellRenderer: nullCheck(renderDecimal(1)),
+    filter: "agNumberColumnFilter",
+  },
+  { headerName: "Resolution", field: "resolution", width: 100 },
+  { headerName: "IPS Screen?", field: "screen-has-ips", width: 70 },
+  { headerName: "Display", field: "display" },
+  {
+    headerName: "Memory Size",
+    field: "memory-size",
+    width: 70,
+    comparator: memComparator,
+  },
+  { headerName: "Storage Type", field: "storage-type", width: 70 },
+  {
+    headerName: "Storage Size",
+    field: "storage-size",
+    width: 70,
+    comparator: storageComparator,
+  },
+  { headerName: "Processor Brand", field: "processor-brand", width: 70 },
+  { headerName: "Processor Range", field: "processor-range", width: 80 },
+  { headerName: "Processor", field: "processor" },
+  { headerName: "Wireless", field: "wlan" },
+  { headerName: "Graphics", field: "graphic-card" },
+  { headerName: "Camera", field: "camera" },
+  { headerName: "Touchscreen?", field: "touch-screen", width: 70 },
+  { headerName: "Storage", field: "storage" },
+  { headerName: "Memory", field: "memory" },
+  { headerName: "Memory Soldered?", field: "memory-soldered", width: 70 },
+  { headerName: "Processor Cache", field: "processor-cache", width: 70 },
+  { headerName: "Battery", field: "battery" },
+  { headerName: "Warranty", field: "warranty" },
+  { headerName: "Weight", field: "weight" },
+  { headerName: "Operating System", field: "operating-system" },
+  {
+    headerName: "Original Price",
+    field: "orig-price",
+    width: 75,
+    filter: "agNumberColumnFilter",
+    cellRenderer: nullCheck(renderDecimal(2)),
+  },
+  {
+    headerName: "Percentage Savings",
+    field: "percentage-savings",
+    width: 75,
+    filter: "agNumberColumnFilter",
+  },
+  { headerName: "Fingerprint Reader", field: "fingerprint-reader" },
+  { headerName: "Product Number", field: "product-number" },
+  { headerName: "Keyboard", field: "keyboard" },
+];
 
+const Grid = ({ data }) => {
   return (
     <div className="ag-theme-balham-dark table-wrapper">
       <AgGridReact
