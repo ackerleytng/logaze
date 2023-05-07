511984214703559675cf104be0e18ecc7e830fee
Ackerley Tng <ackerleytng@gmail.com>
2023-05-07 10:07:59 -0700

    Convert data.js to typescript

--- src/data.ts
+++ src/data.ts
@@ -1,21 +1,23 @@
 import { useState, useEffect } from "react";
 
+export type LaptopData = {[key: string]: string | number};
+
 const storageAddr0 = "https://jsonblob.com/api/jsonBlob/1104252648328282112";
 const storageAddr1 = "https://jsonblob.com/api/jsonBlob/1104252815584542720";
 
-const loadFromStorageLocation = async (location) => {
+const loadFromStorageLocation = async (location: string) => {
     const response = await fetch(location);
     return response.json();
 };
 
-export const loadFromStorage = async () => {
+export const loadFromStorage = async (): Promise<LaptopData[]> => {
     const part0 = await loadFromStorageLocation(storageAddr0);
     const part1 = await loadFromStorageLocation(storageAddr1);
     return part0.concat(part1);
 };
 
-export const useData = () => {
-  const [data, setData] = useState([]);
+export const useData = (): LaptopData[] => {
+  const [data, setData] = useState<LaptopData[]>([]);
 
   useEffect(() => {
     loadFromStorage().then(setData);
@@ -24,7 +26,7 @@ export const useData = () => {
   return data;
 };
 
-export const dataToCsv = (data) => {
+export const dataToCsv = (data: LaptopData[]): string => {
   const header = Object.keys(data[0]);
 
   let csv = data.map((row) =>
@@ -39,7 +41,7 @@ export const dataToCsv = (data) => {
   return csv.join("\r\n");
 };
 
-export const download = (filename, data) => {
+export const download = (filename: string, data: string): void => {
   let element = document.createElement("a");
   element.setAttribute(
     "href",
